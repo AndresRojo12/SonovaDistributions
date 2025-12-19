6afbf05d156194e991dbf697c61822c77841ce2c
Andres <andresrojo20013@gmail.com>
2025-12-19 13:21:59 -0500

    se configuran variables de entorno en el modulo principal #3

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [ContactModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV || '/backend/.env',
+      isGlobal: true,
+    }),
+    ContactModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
